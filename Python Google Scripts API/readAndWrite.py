7ee4742c6d2c6803bdbf1d22d29887e2d07d9eda
Anurag Bansal <anurag.aries@gmail.com>
2020-10-20 15:13:00 -0400

    Update readAndWrite.py

--- Python Google Scripts API/readAndWrite.py
+++ Python Google Scripts API/readAndWrite.py
@@ -16,7 +16,7 @@ creds = service_account.Credentials.from_service_account_file(
 
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1k2eHtnq9Va3VZnTkIsrQKZLVrd-LcZ4mKnF9HLfD6V4'
+SAMPLE_SPREADSHEET_ID = 'enterKEY'
 
 service = build('sheets', 'v4', credentials=creds)
 
