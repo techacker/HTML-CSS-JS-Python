8fd03e35ce2e404fdfd4b177b715a15d6cf84c8e
techacker <anurag.aries@gmail.com>
2020-10-22 00:20:56 -0400

    Removed hackerrank files

--- Hackerrank/exceptions.py
+++
@@ -1,8 +0,0 @@
-# Day 16: Exceptions - Strings to Integer
-
-S = input()
-
-try:
-    print(int(S))
-except:
-    print("Bad String")
--- Hackerrank/splitText.py
+++
@@ -1,5 +0,0 @@
-text = input()
-text = text.replace("o", " ")
-text = text.split()
-text = " o".join(text)
-print(text)
